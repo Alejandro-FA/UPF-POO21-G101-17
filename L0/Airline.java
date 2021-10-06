1928da8e3dc615b74e41e9dce21ce4e0b74b3357
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2021-10-06 18:19:59 +0200

    section 2 upload

---
+++ L0/Airline.java
@@ -0,0 +1,11 @@
+public class Airline {
+    private String name;
+
+    public Airline(String initName){
+        name = initName;
+    }
+
+    public void printName(){
+        System.out.println(name);
+    }
+}
---
+++ L0/TestAirline.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TestAirline {
+    public static void main(String[] args){
+        Airline airline1 = new Airline("SafeNSlow");
+        Airline airline2 = new Airline("CheapNFast");
+
+        LinkedList<Airline> airlineList = new LinkedList<Airline>();
+        airlineList.add(airline1);
+        airlineList.add(airline2);
+    }
+}

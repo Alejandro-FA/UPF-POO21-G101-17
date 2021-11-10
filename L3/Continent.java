fb29338323ffed5dd86b3c83d41751663aa3e673
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2021-11-10 09:27:50 +0100

    Changed PolygonalRegion references to Countries.
    Added getTotalArea method as well.

--- L3/Continent.java
+++ L3/Continent.java
@@ -1,17 +1,22 @@
 import java.util.*;
 
 public class Continent {
-
     /************************ Attributes *************************/
-    private List<PolygonalRegion> countries;
+    private List<Country> countries;
 
     /************************ Constructor ************************/
-    public Continent(List<PolygonalRegion> lst){
-        countries = lst;
+    public Continent(List<Country> countries){
+        this.countries = countries;
     }
 
     /************************ Methods *************************/
     public void draw(java.awt.Graphics g){
-        for (PolygonalRegion c: countries) c.draw(g);
+        for (Country c: countries) c.draw(g);
+    }
+
+    public double getTotalArea(){
+        double totalArea = 0.0;
+        for (Country c: countries) totalArea += c.getArea();
+        return totalArea;
     }
 }

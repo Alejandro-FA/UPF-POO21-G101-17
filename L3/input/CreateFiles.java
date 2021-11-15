c06ed7e340dc713a77e16560188523149bd2ac1f
marcagve18 <78203527+marcagve18@users.noreply.github.com>
2021-11-15 18:41:40 +0100

    Debugging

--- L3/input/CreateFiles.java
+++ L3/input/CreateFiles.java
@@ -1,8 +1,8 @@
 package input;
 public class CreateFiles {
     public static void main(String[] args) {
-        // BorderFiles.json2CSV("assets/countries.geojson");
+        BorderFiles.json2CSV("assets/countries.geojson");
         boolean append = false;
-        CityFiles.writeToAll(append, "Capital City", 12, -85.0, 179.9);
+        // CityFiles.writeToAll(append, "Capital City", 12, -85.0, 179.9);
     }
 }

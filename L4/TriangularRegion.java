b3975f901f7bb30d9a20d7d9bfd17fdf1ef973f4
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2021-11-25 09:01:50 +0100

    Triangular getArea method added

--- L4/TriangularRegion.java
+++ L4/TriangularRegion.java
@@ -13,11 +13,10 @@ public class TriangularRegion extends PolygonalRegion{
         this.p3 = p3;
     }
 
-    // public double getArea() {
-    //     Vector side1 = p1.difference(p2);
-    //     Vector side2 = p3.difference(p3);
-    //     double height = 
-
-    //     return p1.getX()*p2.getY();
-    // }
+    public double getArea() {
+        double base = p1.difference(p2).magnitude() / 2.0; // We divide by two here for efficiency
+        double hypotenuse = p3.difference(p1).magnitude();
+        double height = Math.sqrt( Math.pow(hypotenuse, 2) - Math.pow(base, 2) );
+        return base*height;
+    }
 }

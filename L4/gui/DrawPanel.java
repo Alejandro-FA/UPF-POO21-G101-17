388c4c8d0de259598bac67f0910a68c30726b722
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2021-11-30 15:09:23 +0100

    Changes to select entities implemented

--- L4/gui/DrawPanel.java
+++ L4/gui/DrawPanel.java
@@ -1,16 +1,18 @@
 package gui;
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.event.*;
 import javax.swing.*;
-
-import entities.Entity;
-
+import entities.*;
 import java.util.*;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements MouseListener{
 	protected LinkedList< Entity > drawables;
+	protected LinkedList< Entity > selection;
 
 	public DrawPanel() {
 		drawables = new LinkedList< Entity >();
+		selection = new LinkedList< Entity >();
+		addMouseListener( this );
 	}
 
 	public void addDrawable( Entity entity ) {
@@ -24,7 +26,26 @@ public class DrawPanel extends JPanel {
 	}
 
 	public void translate( int dx, int dy ) {
-		for ( int i = 0; i < drawables.size(); ++i )
-			drawables.get( i ).translate( dx, dy );
+		for ( int i = 0; i < selection.size(); ++i )
+			selection.get( i ).translate( dx, dy );
 	}
+
+	/************************ MouseListener Methods ****************************/
+	public void mousePressed(MouseEvent e) {
+		Point p = new Point( e.getX(), e.getY() );
+		for (Entity entity: drawables) {
+			// Only applicable to regions
+			if (entity instanceof Region) {
+				Region region = (Region) entity;
+				boolean isClicked = region.isPointInside(p);
+				boolean isSelected = selection.contains(region);
+				if (isClicked && !isSelected) selection.add(region);
+				else if (isClicked && isSelected) selection.remove(region);
+			}
+		}
+	}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
 }
